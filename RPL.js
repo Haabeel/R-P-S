5c0ab8b5c767577d691b020de0c888c5f9c9597d
Khoalah <112350051+Khoalah@users.noreply.github.com>
2022-09-28 20:40:57 +0400

    Add files via upload

--- RPL.js
+++ RPL.js
@@ -1,17 +1,35 @@
 let RPLarray = ["Rock", "Paper", "Scissor"];
+
+let wrCount = 0;
+let trCount = 0;
+let lrCount = 0;
+
 const scoreTitle = document.querySelector(".wonLost");
+
 const PchoiceBG = document.querySelector(".Pchoice");
 const CchoiceBG = document.querySelector(".Cchoice");
-function getComputerChoice() {
-  return RPLarray[Math.floor(Math.random() * 3)];
-}
+
+const R = document.querySelector(".Rock");
+const P = document.querySelector(".Paper");
+const S = document.querySelector(".Scissor");
+const G = document.querySelector(".GameOver");
+const W = document.querySelector(".wonText");
+const L = document.querySelector(".lostText");
+
+const scoreCount = document.querySelectorAll(".ScoreCount");
+const Lscore = scoreCount[1];
+const Wscore = scoreCount[0];
+
 const startbtn = document.querySelector(".reveal-btn");
+const restartbtncontainer = document.querySelector(".RestartScore");
 const mainstartbtn = document.querySelector(".main-btn");
+const restartbtn = document.querySelector(".hidden-content");
+
+restartbtn.addEventListener("click", restart);
 mainstartbtn.addEventListener(
   "click",
   () => {
     mainstartbtn.classList.add("main-start-btn");
-    //startbtn.classList.remove("main-start-btn");
     getPlayerChoice();
     restart();
     scoreTitles();
@@ -20,16 +38,14 @@ mainstartbtn.addEventListener(
     once: true,
   }
 );
+
+function getComputerChoice() {
+  return RPLarray[Math.floor(Math.random() * 3)];
+}
+
 function start() {
   startbtn.addEventListener("click", getPlayerChoice);
 }
-const restartbtn = document.querySelector(".hidden-content");
-restartbtn.addEventListener("click", restart);
-
-const restartbtncontainer = document.querySelector(".RestartScore");
-let wrCount = 0;
-let trCount = 0;
-let lrCount = 0;
 
 function scoreTitles() {
   if (scoreTitle.classList.contains("hide-cards")) {
@@ -40,11 +56,14 @@ function scoreTitles() {
     scoreTitle.classList.add("hide-cards");
   }
 }
+
 function restart() {
   restartbtncontainer.classList.remove("hidden-content");
   wrCount *= 0;
   lrCount *= 0;
+
   getPlayerChoice();
+
   R.classList.remove("hidden-content");
   P.classList.remove("hidden-content");
   S.classList.remove("hidden-content");
@@ -62,8 +81,10 @@ function restart() {
   } else if (CchoiceBG.classList.contains("RockBG")) {
     CchoiceBG.classList.remove("RockBG");
   }
+
   CchoiceBG.classList.add("defaultChoiceContainerBGcolor");
   CchoiceBG.classList.remove("white");
+
   if (PchoiceBG.classList.contains("PaperBG")) {
     PchoiceBG.classList.remove("PaperBG");
   } else if (PchoiceBG.classList.contains("ScissorBG")) {
@@ -71,6 +92,7 @@ function restart() {
   } else if (PchoiceBG.classList.contains("RockBG")) {
     PchoiceBG.classList.remove("RockBG");
   }
+
   PchoiceBG.classList.add("defaultChoiceContainerBGcolor");
   PchoiceBG.classList.remove("white");
 }
@@ -80,20 +102,12 @@ function getPlayerChoice() {
   const btnR = btn[0];
   const btnP = btn[1];
   const btnS = btn[2];
+
   btnR.addEventListener("click", getPlayerChoiceR);
   btnP.addEventListener("click", getPlayerChoiceP);
   btnS.addEventListener("click", getPlayerChoiceS);
 }
-const R = document.querySelector(".Rock");
-const P = document.querySelector(".Paper");
-const S = document.querySelector(".Scissor");
-const G = document.querySelector(".GameOver");
-const W = document.querySelector(".wonText");
-const L = document.querySelector(".lostText");
-const scoreCount = document.querySelectorAll(".ScoreCount");
-const Lscore = scoreCount[1];
-const Wscore = scoreCount[0];
-//scoreTitles();
+
 function won() {
   R.classList.add("hidden-content");
   P.classList.add("hidden-content");
@@ -102,35 +116,6 @@ function won() {
   W.classList.remove("hide-cards");
   scoreTitle.classList.remove("flex");
   scoreTitle.classList.add("hide-cards");
-  //scoreTitles();
-  if (CchoiceBG.classList.contains("PaperBG")) {
-    CchoiceBG.classList.remove("PaperBG");
-  } else if (CchoiceBG.classList.contains("ScissorBG")) {
-    CchoiceBG.classList.remove("ScissorBG");
-  } else if (CchoiceBG.classList.contains("RockBG")) {
-    CchoiceBG.classList.remove("RockBG");
-  }
-  CchoiceBG.classList.add("defaultChoiceContainerBGcolor");
-  CchoiceBG.classList.remove("white");
-  if (PchoiceBG.classList.contains("PaperBG")) {
-    PchoiceBG.classList.remove("PaperBG");
-  } else if (PchoiceBG.classList.contains("ScissorBG")) {
-    PchoiceBG.classList.remove("ScissorBG");
-  } else if (PchoiceBG.classList.contains("RockBG")) {
-    PchoiceBG.classList.remove("RockBG");
-  }
-  PchoiceBG.classList.add("defaultChoiceContainerBGcolor");
-  PchoiceBG.classList.remove("white");
-}
-function lost() {
-  R.classList.add("hidden-content");
-  P.classList.add("hidden-content");
-  S.classList.add("hidden-content");
-  G.classList.remove("hide-cards");
-  L.classList.remove("hide-cards");
-  scoreTitle.classList.remove("flex");
-  scoreTitle.classList.add("hide-cards");
-  //scoreTitles();
 
   if (CchoiceBG.classList.contains("PaperBG")) {
     CchoiceBG.classList.remove("PaperBG");
@@ -139,8 +124,10 @@ function lost() {
   } else if (CchoiceBG.classList.contains("RockBG")) {
     CchoiceBG.classList.remove("RockBG");
   }
+
   CchoiceBG.classList.add("defaultChoiceContainerBGcolor");
   CchoiceBG.classList.remove("white");
+
   if (PchoiceBG.classList.contains("PaperBG")) {
     PchoiceBG.classList.remove("PaperBG");
   } else if (PchoiceBG.classList.contains("ScissorBG")) {
@@ -148,72 +135,126 @@ function lost() {
   } else if (PchoiceBG.classList.contains("RockBG")) {
     PchoiceBG.classList.remove("RockBG");
   }
+
   PchoiceBG.classList.add("defaultChoiceContainerBGcolor");
   PchoiceBG.classList.remove("white");
 }
+
+function lost() {
+  R.classList.add("hidden-content");
+  P.classList.add("hidden-content");
+  S.classList.add("hidden-content");
+  G.classList.remove("hide-cards");
+  L.classList.remove("hide-cards");
+  scoreTitle.classList.remove("flex");
+  scoreTitle.classList.add("hide-cards");
+
+  if (CchoiceBG.classList.contains("PaperBG")) {
+    CchoiceBG.classList.remove("PaperBG");
+  } else if (CchoiceBG.classList.contains("ScissorBG")) {
+    CchoiceBG.classList.remove("ScissorBG");
+  } else if (CchoiceBG.classList.contains("RockBG")) {
+    CchoiceBG.classList.remove("RockBG");
+  }
+
+  CchoiceBG.classList.add("defaultChoiceContainerBGcolor");
+  CchoiceBG.classList.remove("white");
+
+  if (PchoiceBG.classList.contains("PaperBG")) {
+    PchoiceBG.classList.remove("PaperBG");
+  } else if (PchoiceBG.classList.contains("ScissorBG")) {
+    PchoiceBG.classList.remove("ScissorBG");
+  } else if (PchoiceBG.classList.contains("RockBG")) {
+    PchoiceBG.classList.remove("RockBG");
+  }
+
+  PchoiceBG.classList.add("defaultChoiceContainerBGcolor");
+  PchoiceBG.classList.remove("white");
+}
+
 function CAddRockBG() {
   CchoiceBG.classList.remove("defaultChoiceContainerBGcolor");
+
   if (CchoiceBG.classList.contains("PaperBG")) {
     CchoiceBG.classList.remove("PaperBG");
   } else if (CchoiceBG.classList.contains("ScissorBG")) {
     CchoiceBG.classList.remove("ScissorBG");
   }
+
   CchoiceBG.classList.add("RockBG");
   CchoiceBG.classList.add("white");
 }
+
 function CaddPaperBG() {
   CchoiceBG.classList.remove("defaultChoiceContainerBGcolor");
+
   if (CchoiceBG.classList.contains("RockBG")) {
     CchoiceBG.classList.remove("RockBG");
   } else if (CchoiceBG.classList.contains("ScissorBG")) {
     CchoiceBG.classList.remove("ScissorBG");
   }
+
   CchoiceBG.classList.add("PaperBG");
   CchoiceBG.classList.add("white");
 }
+
 function CaddScissorBG() {
   CchoiceBG.classList.remove("defaultChoiceContainerBGcolor");
+
   if (CchoiceBG.classList.contains("PaperBG")) {
     CchoiceBG.classList.remove("PaperBG");
   } else if (CchoiceBG.classList.contains("RockBG")) {
     CchoiceBG.classList.remove("RockBG");
   }
+
   CchoiceBG.classList.add("ScissorBG");
   CchoiceBG.classList.add("white");
 }
+
 function PaddRockBG() {
   PchoiceBG.classList.remove("defaultChoiceContainerBGcolor");
+
   if (PchoiceBG.classList.contains("PaperBG")) {
     PchoiceBG.classList.remove("PaperBG");
   } else if (PchoiceBG.classList.contains("ScissorBG")) {
     PchoiceBG.classList.remove("ScissorBG");
   }
+
   PchoiceBG.classList.add("RockBG");
   PchoiceBG.classList.add("white");
 }
+
 function PaddPaperBG() {
   PchoiceBG.classList.remove("defaultChoiceContainerBGcolor");
+
   if (PchoiceBG.classList.contains("RockBG")) {
     PchoiceBG.classList.remove("RockBG");
   } else if (PchoiceBG.classList.contains("ScissorBG")) {
     PchoiceBG.classList.remove("ScissorBG");
   }
+
   PchoiceBG.classList.add("PaperBG");
   PchoiceBG.classList.add("white");
 }
+
 function PaddScissorBG() {
   PchoiceBG.classList.remove("defaultChoiceContainerBGcolor");
+
   if (PchoiceBG.classList.contains("PaperBG")) {
     PchoiceBG.classList.remove("PaperBG");
   } else if (PchoiceBG.classList.contains("RockBG")) {
     PchoiceBG.classList.remove("RockBG");
   }
+
   PchoiceBG.classList.add("ScissorBG");
   PchoiceBG.classList.add("white");
 }
+
 function getPlayerChoiceR() {
   computerChoice = getComputerChoice();
+
   PaddRockBG();
+
   if (computerChoice == "Rock") {
     CAddRockBG();
     trCount++;
@@ -226,6 +267,7 @@ function getPlayerChoiceR() {
     wrCount++;
     Wscore.textContent = wrCount;
   }
+
   if (wrCount >= 5) {
     won();
     wrCount *= 0;
@@ -234,9 +276,12 @@ function getPlayerChoiceR() {
     lrCount *= 0;
   }
 }
+
 function getPlayerChoiceP() {
   computerChoice = getComputerChoice();
+
   PaddPaperBG();
+
   if (computerChoice == "Rock") {
     CAddRockBG();
     wrCount++;
@@ -249,6 +294,7 @@ function getPlayerChoiceP() {
     lrCount++;
     Lscore.textContent = lrCount;
   }
+
   if (wrCount >= 5) {
     won();
     wrCount *= 0;
@@ -260,7 +306,9 @@ function getPlayerChoiceP() {
 
 function getPlayerChoiceS() {
   PaddScissorBG();
+
   computerChoice = getComputerChoice();
+
   if (computerChoice == "Rock") {
     CAddRockBG();
     lrCount++;
@@ -273,6 +321,7 @@ function getPlayerChoiceS() {
     CaddScissorBG();
     trCount++;
   }
+
   if (wrCount >= 5) {
     won();
     wrCount *= 0;
